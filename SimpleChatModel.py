53715e193bcd7a5f7981ce2dc78c41e57ecbc139
rudy <rudymikiya@gmail.com>
2024-08-21 22:21:29 +0800

    Simple chat model

---
+++ SimpleChatModel.py
@@ -0,0 +1,43 @@
+import os
+
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import BaseOutputParser
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
+
+# Set proxy if it is needed...........
+proxy = 'http://localhost:7897'
+os.environ['http_proxy'] = proxy
+os.environ['https_proxy'] = proxy
+
+# read the api key from MyApiKey.txt
+with open('MyApiKey.txt', 'r') as file:
+    api_key = file.read().strip()
+    print(f"API key: {api_key}")
+    os.environ['GOOGLE_API_KEY'] = api_key
+
+
+class CommaSeparatedListOutputParser(BaseOutputParser):
+    """Parse the output of an LLM call to a comma-separated list."""
+
+    def parse(self, text: str):
+        """Parse the output of an LLM call."""
+        return text.strip().split(", ")
+
+
+if __name__ == "__main__":
+    template = """You are a helpful assistant who generates comma separated lists.
+    A user will pass in a category, and you should generate 5 objects in that category in a comma separated list.
+    ONLY return a comma separated list, and nothing more."""
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_template = "{text}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+    chain = (chat_prompt | ChatGoogleGenerativeAI(
+        model="models/gemini-1.5-flash-latest") | CommaSeparatedListOutputParser())
+    res = chain.invoke({"text": "colors"})
+    print(res)
